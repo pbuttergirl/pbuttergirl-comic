0775719745d2faad7ed810af58c4012ba48265b4
ButterflyBug <49125893+ButterflyBug@users.noreply.github.com>
2021-11-12 13:30:59 +0100

    Add deployment on netlify (#10)
    * Add deployment on netlify
    
    Update location of episodes in the project
    Update github action with required node version

--- utils/episodes-handlers.tsx
+++ utils/episodes-handlers.tsx
@@ -4,16 +4,16 @@ import { groupBy } from "lodash";
 export type EpisodeType = ReturnType<typeof getEpisodes>[0];
 
 export function getEpisodes() {
-  const episodes = glob.sync("./episodes/episode-*/*.png");
+  const episodes = glob.sync("./public/episodes/episode-*/*.png");
   const groupedList = groupBy(episodes, groupFunction);
   return Object.entries(groupedList).map(([key, value]) => {
     return {
       name: (key.charAt(0).toUpperCase() + key.slice(1)).replace("-", " "),
-      images: value,
+      images: value.map((item) => item.replace("public/", "")),
     };
   });
 }
 
 export const groupFunction = (episodePath: string) => {
-  return episodePath.split("/")[2];
+  return episodePath.split("/")[3];
 };
