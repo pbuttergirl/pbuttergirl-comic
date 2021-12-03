006db1b142facd1a70c8180ddfae2c01391426f9
Katarzyna Szuper <kszuper1994@gmail.com>
2021-12-03 16:42:51 +0100

    Introduce Google Analytics
    The scripts were added to `_app.tsx` file.
    Environment variable was added to `.env` file.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,45 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import * as gtag from '../analytics/gtag';
+import { useRouter } from 'next/dist/client/router';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  console.log(router);
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname
+            });`,
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 export default MyApp;
