2fb81ec93503e19c106a501c0d4ebbd078a4d604
Katarzyna Szuper <kszuper1994@gmail.com>
2021-12-06 13:41:59 +0100

    Update the test for page title
    `Head` tag provided by Nextjs was replaced with `Helmet` from
    `react-helmet` library because it makes it easier to test the title
    and meta tags.

--- __tests__/[slug].test.tsx
+++ __tests__/[slug].test.tsx
@@ -6,10 +6,10 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import EpisodePage from '../pages/episodes/[slug]';
 import { getEpisodes } from '../utils/episodes-handlers';
-import { Container } from 'react-bootstrap';
+import { Helmet } from 'react-helmet';
 
 describe('EpisodePage', () => {
-  it('renders a heading', () => {
+  beforeEach(() => {
     const episode = getEpisodes()[0];
     const episodes = getEpisodes();
     render(
@@ -19,7 +19,9 @@ describe('EpisodePage', () => {
         episodes={episodes}
       />
     );
+  });
 
+  it('renders a heading', () => {
     const heading = screen.getByRole('heading', {
       name: /Episode 1/i,
     });
@@ -31,18 +33,15 @@ describe('EpisodePage', () => {
   });
 
   it('renders a title', async () => {
-    const episode = getEpisodes()[0];
-    const episodes = getEpisodes();
-    const { container } = render(
-      <EpisodePage
-        episode={episode}
-        currentEpisodeNumber={1}
-        episodes={episodes}
-      />
-    );
+    const helmet = Helmet.peek();
 
-    await waitFor(() => {
-      expect(container.querySelector('head')).toMatchSnapshot();
-    });
+    expect(helmet.title).toEqual(['Episode 1', ' - Peanutbutter girl comic']);
+  });
+
+  it('renders meta tags', () => {
+    const helmet = Helmet.peek();
+    const metaTags = helmet.metaTags;
+
+    expect(metaTags).toEqual([{ name: 'description', content: 'Episode 1' }]);
   });
 });
