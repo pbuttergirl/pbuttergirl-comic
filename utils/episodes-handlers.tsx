04d48348497e53ade8776b962b72954cb736644a
Katarzyna Szuper <kszuper1994@gmail.com>
2021-12-09 12:39:22 +0100

    Handle displaying images in correct order
    Before that change, images were sorted alphabetically.
    The names of images were adjusted by adding number as prefix
    which indicates the position in the order - logical order.

--- utils/episodes-handlers.tsx
+++ utils/episodes-handlers.tsx
@@ -7,9 +7,10 @@ export function getEpisodes() {
   const episodes = glob.sync('./public/episodes/episode-*/*.png');
   const groupedList = groupBy(episodes, groupFunction);
   return Object.entries(groupedList).map(([key, value]) => {
+    const images = value.map(item => item.replace('./public/', '/')).sort();
     return {
       name: (key.charAt(0).toUpperCase() + key.slice(1)).replace('-', ' '),
-      images: value.map(item => item.replace('./public/', '/')),
+      images,
     };
   });
 }
