b46da94ae52020341d61b1721866d79673c859ae
Katarzyna Szuper <kszuper1994@gmail.com>
2021-12-21 12:41:43 +0100

    Adjust tailwind configuration according to the latest update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
