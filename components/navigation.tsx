66ee9b56a3a6ca8a6db1363a46b2ad2af07ae4b5
Katarzyna Szuper <kszuper1994@gmail.com>
2022-02-06 14:05:35 +0100

    Improve aria label for navigation

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -36,15 +36,17 @@ export const Arrow = (props: ArrowProps) => {
 
   const path = direction === 'left' ? previousEpisodePath : nextEpisodePath;
   const testId = `${direction}-arrow`;
+  const navigationLabelLeft = 'Navigation to the previous episode.';
+  const navigationLabelRight = 'Navigation to the next episode.';
 
   if (disabled) {
     return (
       <a
         className="pointer-events-none"
         data-testid={testId}
-        // aria-label={
-        //   'The first or lat episode reached. Button disabled - navigation impossible.'
-        // }
+        aria-label={
+          'The first or last episode reached. Button disabled - navigation impossible.'
+        }
       >
         <Icon className={`h-7 w-7 sm:h-8 sm:w-8 text-black-500 opacity-25`} />
       </a>
@@ -54,7 +56,9 @@ export const Arrow = (props: ArrowProps) => {
       <Link href={path(currentEpisode)} passHref>
         <a
           data-testid={testId}
-          aria-label={'Navigation to the next of previous episode.'}
+          aria-label={
+            direction === 'left' ? navigationLabelLeft : navigationLabelRight
+          }
         >
           <Icon className={`h-7 w-7 sm:h-8 sm:w-8 text-black-500`} />
         </a>
