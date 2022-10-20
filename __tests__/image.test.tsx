ec67b4e7ef648023f9a27b4cb59c15eabc3376e9
Katarzyna Szuper <kszuper1994@gmail.com>
2022-10-20 07:18:44 +0200

    Remove unnecessery test case

--- __tests__/image.test.tsx
+++ __tests__/image.test.tsx
@@ -14,17 +14,4 @@ fdescribe(ComicImage, () => {
     const comicImage = screen.getByRole('img');
     expect(comicImage).toHaveAttribute('alt', altText);
   });
-
-  it('renders second episode with its alt attribute', () => {
-    const altText =
-      "Second episode shows two girls standing close to each other. They are the best friends. They have a make up, nice hair and they are ready to go for a party. They are taking a selfie number 435 but they do not like any of them. They try one more time. The girl with blond hair says: 'This time will be perfect.' ";
-    render(
-      <ComicImage
-        imagePath={'/episodes/episode-2/Episode2.png'}
-        altText={altText}
-      />
-    );
-    const comicImage = screen.getByRole('img');
-    expect(comicImage).toHaveAttribute('alt', altText);
-  });
 });
